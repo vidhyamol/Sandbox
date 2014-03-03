09eb33993425ff4f47296e3b602b9e8c1c61f78d
vidhyamol <vidhyamol11@gmail.com>
2014-03-03 21:12:19 +0530

    my commit

---
+++ test.py
@@ -0,0 +1,31 @@
+from bottle import route,run,template,get,post,put,delete,request
+pat_dic={}
+
+@post('/patient')
+def add_data():
+	id=request.POST['id']
+	name=request.POST['name']
+	gender=request.POST['gender']
+	info_dic={'name':name,'gender':gender}
+	pat_dic[id]=info_dic
+	return pat_dic
+@get('/patient/<id>')
+def get_data(id):
+	return pat_dic[id]
+	
+@put('/patient/<id>')
+def modify(id):
+	if id in pat_dic.keys():
+		pat_dic[id].update(name = request.POST['name'],gender = request.POST['gender'])
+		return"successfully updated"
+	else:
+		return"Error in id"
+	
+@delete('/patient/del/<id>')
+def remove(id):
+	
+	del(pat_dic[id])
+	return pat_dic
+run(host='localhost',port=8080)
+
+			
